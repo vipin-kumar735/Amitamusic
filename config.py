0b76c6d53ab49190be8c02f0ba66eb72e636b0a7
vipin-kumar735 <patelharimohan31@gmail.com>
2025-11-13 16:20:55 +0530

    𝐕𝐈𝐏 𝐂𝐇𝐎𝐑

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TEAMPURVI/PURVI_MUSIC",
+    "https://github.com/vipin-kumar735/Amitamusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
